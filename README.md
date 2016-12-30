d5ffad685b9ea13c15253071b2c96f673840d1f1
mafallahi <mohammad.a.f.1374@hotmail.com>
2016-12-30 17:06:24 +0330

    initial commit :tada:

---
+++ README.md
@@ -0,0 +1,12 @@
+##Math Game
+>Simple game play with Mathematics ||
+live demo [here](http://mafallahi.github.io)
+
+
+##install package for developer :chicken:	
+  ```npm install```
+
+##browser auto reload :chicken:  
+``gulp``
+#Licence
+MIT
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+
+
+gulp.task('default', function() {
+    browserSync.init({
+        server:{
+            baseDir:("./")    
+        }
+    });
+
+    gulp.watch('src/**/*',browserSync.reload);
+    gulp.watch('*.html',browserSync.reload); 
+});
